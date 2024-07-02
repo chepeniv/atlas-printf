beaa5f97a503a7570c9c2cfb177a93ec5b4699f3
chepeniv <jose.olmos@atlasschool.com>
2024-07-01 21:58:16 -0500

    [passing] style: minimized number of lines

--- printf.c
+++ printf.c
@@ -5,15 +5,15 @@
 /**
  * _printf - the main function that aims to recreate
  * the standard printf function.
- * 
+ *
  * Description: Function _printf is designed to mimic standard printf.
  * It is built to handle specifying tokens %s, %d, %c, and %i.
  * The function iterates through the input string character by character.
  * It handles special characters depending on their type. (as indicated by
  * the character specifiers)
- * 
+ *
  * @fstr: a pointer to the format string which the _printf function acts upon
- * 
+ *
  * Return: returns the number of characters printed, not including the '\0'
  */
 
@@ -26,29 +26,22 @@ int _printf(const char *fstr, ...)
 
 	if (fstr == NULL)
 		return (-1);
-
 	current = fstr[pos];
 	if (current == '\0')
 		return (0);
-
 	va_start(flist, fstr);
 	while (current != '\0')
 	{
 		if (current == '%')
 		{
 			next = fstr[pos + 1];
-			if (next == 'c' ||
-				next == 's' ||
-				next == 'd' ||
-				next == 'i')
+			if (next == 'c' || next == 's' || next == 'd' || next == 'i')
 			{
 				param = va_arg(flist, void *);
 				total += handle_format(next, param);
 			}
 			else if (next == '\0')
-			{
 				return (-1);
-			}
 			else
 			{
 				total += _putchar(current);
@@ -59,11 +52,9 @@ int _printf(const char *fstr, ...)
 		}
 		else
 			total += _putchar(current);
-
 		pos++;
 		current = fstr[pos];
 	}
-
 	va_end(flist);
 	return (total);
 }
