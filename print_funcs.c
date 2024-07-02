6d20851b51ac51b5f73e60bd2da8f947e45ab648
solarianvulpine <victoria.teale222@gmail.com>
2024-07-02 01:14:18 +0000

    Betty: removed trailing whitespace in print_str documentation

--- print_funcs.c
+++ print_funcs.c
@@ -36,9 +36,9 @@ int print_char(void *param)
  * if the string is NULL it prints "(null)"
  * This function iterates through the string one character at a time
  * and prints them using _putchar.
- * 
+ *
  * @param: the parameter for print_str; a pointer to the string to be printed
- * 
+ *
  * Return: The function return the number of characters printed
  */
 int print_str(void *param)
